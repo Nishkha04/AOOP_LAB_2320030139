34d5e28cf4bb634246c8d0473a85e14d6f7944a2
T NISHKHA <tammareddinishkha04@gmail.com>
2024-08-25 01:09:33 +0530

    Add files via upload

---
+++ Library_Management/src/Com/Library_Management/specialmember.java
@@ -0,0 +1,9 @@
+package Com.Library_Management;
+
+public class specialmember extends member {
+	    public specialmember(String name, String memberId) {
+	        super(name, memberId);
+	    }
+
+	   
+	}
